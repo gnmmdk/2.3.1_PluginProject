4be94ab3a783bde1bc38bf17df294d3b85c21690
jianjian <jj.kang@zkteco.com>
2019-12-31 17:25:05 +0800

    代码流程笔记

--- app/src/main/java/com/kangjj/plugin/PluginManager.java
+++ app/src/main/java/com/kangjj/plugin/PluginManager.java
@@ -95,6 +95,7 @@ public class PluginManager {
 
     /**
      *    反射5.0 系统源码，来解析apk文件里面的所有信息 9.0的源码也可以用 不过手机是8.0的系统 应该没有影响。
+     *
      */
     public void parserApkAction(){
         try {
@@ -103,31 +104,36 @@ public class PluginManager {
                 Log.d(TAG, "插件包 不存在...");
                 return ;
             }
+
             //实例化PackageParser对象
             Class mPackageParserClass = Class.forName("android.content.pm.PackageParser");
             Object mPackageParser= mPackageParserClass.newInstance();
-            // 1 执行此方法  public Package parsePackage(File packageFile, int flags)  就是为了，拿到Package
+            //todo 1  执行此方法  public Package parsePackage(File packageFile, int flags)  就是为了，拿到Package
+            // Package 可以理解为AndroidManifest了。
             Method mParsePackageMethod = mPackageParserClass.getMethod("parsePackage",File.class,int.class);
             Object mPackage = mParsePackageMethod.invoke(mPackageParser,file, PackageManager.GET_ACTIVITIES);
             // 继续分析Package
-            // 得到receivers    public final ArrayList<Activity> receivers = new ArrayList<Activity>(0);
+            // todo 2 得到receivers    public final ArrayList<Activity> receivers = new ArrayList<Activity>(0);
+            //  这里的receivers对应到了<receiver android:name=".StaticReceiver">
             Field  mReceiversField=mPackage.getClass().getDeclaredField("receivers");   //getField 获取一个类的 ==public成员变量，包括基类== 。 getDeclaredField 获取一个类的 ==所有成员变量，不包括基类== 。
+
             //receivers 的本质是 ArrayList<Activity> 这里的Activity是PackageParser里面的内部类
             Object receivers = mReceiversField.get(mPackage);
             ArrayList arrayList = (ArrayList) receivers;
-            // 此Activity 不是组件的Activity，是PackageParser里面的内部类
+            // todo 此Activity 不是组件的Activity，是PackageParser里面的内部类
             for (Object mActivity : arrayList) { // mActivity --> <receiver android:name=".StaticReceiver">
                 // 获取 <intent-filter>    intents== 很多 Intent-Filter
-                // 通过反射拿到intents
+                // todo 3 通过反射拿到IntentFilter 该IntentFilter在PackageParser$Component中，是Activity的父类
                 Class mComponentClass= Class.forName("android.content.pm.PackageParser$Component");
                 Field intentsField=mComponentClass.getDeclaredField("intents");  // public final ArrayList<II> intents;
                 ArrayList<IntentFilter> intents = (ArrayList) intentsField.get(mActivity);  // intents 所属的对象是谁 ?   ->mActivity
-                //至此拿到了<intent-filter>，还需要拿到 android:name=".StaticReceiver"
+                //todo 4 至此拿到了<intent-filter>，还需要拿到 android:name=".StaticReceiver"
                 // activityInfo.name; == android:name=".StaticReceiver"
-                // 分析源码 如何 拿到 ActivityInfo
-                /*Field mActivityInfoField = mActivity.getClass().getDeclaredField("info");   //这种方式也可以拿到
-                ActivityInfo mActivityInfo= (ActivityInfo) mActivityInfoField.get(mActivity); */
+                // 分析源码 如何 拿到 ActivityInfo  Activity->ActivityInfo info
+                /* todo 4.1 Field mActivityInfoField = mActivity.getClass().getDeclaredField("info");   //这种方式也可以拿到
+                    ActivityInfo mActivityInfo= (ActivityInfo) mActivityInfoField.get(mActivity); */
 
+                // todo 4.3 执行UserHandle内部的 getCallingUserId方法，获取到UserId。
                 Class UserHandleClass = Class.forName("android.os.UserHandle");
                 Method getCallingUserIdMethod = UserHandleClass.getMethod("getCallingUserId");
                 int userId = (int) getCallingUserIdMethod.invoke(null);
@@ -136,9 +142,11 @@ public class PluginManager {
                 // public static final ActivityInfo generateActivityInfo(Activity a, int flags, PackageUserState state, int userId)
                 Method generateActivityInfoMethod = mPackageParserClass.getMethod("generateActivityInfo",mActivity.getClass(),int.class, mPackageUserStateClass,int.class);
 //                generateActivityInfoMethod.setAccessible(true);//这里可以去掉 因为该方法是public
+                //todo 4.2 执行PackageParser的generateActivityInfo方法获取到ActivityInfo。如何获取userId？
                 ActivityInfo mActivityInfo = (ActivityInfo) generateActivityInfoMethod.invoke(null,mActivity,0,mPackageUserStateClass.newInstance(),userId);
                 String receiverClassName = mActivityInfo.name;//com.kangjj.plugin.packages.StaticReceiver
 
+                //todo 5 使用类加载去实例化 BroadcastReceiver，然后就可以注册广播了。
                 Class mStaticReceiverClass = getClassLoader().loadClass(receiverClassName);
                 BroadcastReceiver broadcastReceiver = (BroadcastReceiver) mStaticReceiverClass.newInstance();
                 for (IntentFilter intentFilter : intents) {
